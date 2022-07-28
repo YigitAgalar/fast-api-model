afb29c9b4076bd140c70d99754dbf2caf780bba3
Yiğit Ağalar <45517321+YigitAgalar@users.noreply.github.com>
2022-07-28 11:39:20 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,64 +1,62 @@
 from multiprocessing.sharedctypes import Value
 from fastapi import FastAPI
-from pydantic import BaseModel, root_validator,validator
+from fastapi.encoders import jsonable_encoder
+from sklearn.linear_model import LogisticRegression
+from pydantic import BaseModel, root_validator, validator
+import pickle
 import pandas as pd
+from data.data import preprocessor
 
-app= FastAPI()
+app = FastAPI()
 
+loaded_model = pickle.load(open("models\model.pkl", "rb"))
 
 
 class Data(BaseModel):
-    tenure:int
-    MonthlyCharges:float
-    TotalCharges:float
-    SeniorCitizen:int
-    gender:str
-    Partner:str
-    Dependents:str
-    PhoneService:str         
-    MultipleLines:str        
-    InternetService:str      
-    OnlineSecurity:str       
-    OnlineBackup:str         
-    DeviceProtection:str     
-    TechSupport:str         
-    StreamingTV:str        
-    StreamingMovies: str     
-    Contract:str             
-    PaperlessBilling:str     
-    PaymentMethod:str        
+    customerID: int
+    tenure: float
+    MonthlyCharges: float
+    TotalCharges: float
+    SeniorCitizen: int
+    gender: int
+    Partner: int
+    Dependents: int
+    PhoneService: int
+    MultipleLines: int
+    InternetService: int
+    OnlineSecurity: int
+    OnlineBackup: int
+    DeviceProtection: int
+    TechSupport: int
+    StreamingTV: int
+    StreamingMovies: int
+    Contract: int
+    PaperlessBilling: int
+    PaymentMethod: int
 
-
-    @root_validator()
-    def validate_all(cls,values):
-        if values["SeniorCitizen"] == "" or " " in values["SeniorCitizen"] :
-            raise ValueError("SeniorCitizen can't be null")
-        if values["Partner"] =="":
-            raise ValueError("Partner can't be null")
-
-
-  #'Partner', 'Dependents',
-   #          'InternetService','OnlineSecurity', 'OnlineBackup', 'DeviceProtection', 'TechSupport',
+    #'Partner', 'Dependents',
+    #          'InternetService','OnlineSecurity', 'OnlineBackup', 'DeviceProtection', 'TechSupport',
     #       'StreamingTV', 'StreamingMovies', 'Contract', 'PaperlessBilling',
-     #      'PaymentMethod']
-   
+    #      'PaymentMethod']
+
     @validator("MonthlyCharges")
-    def validate_tenure(cls,value):
+    def validate_tenure(cls, value):
         if value <= 0:
             raise ValueError("MonthlyCharges >0 olmalı")
         return value
-    
 
 
 @app.get("/")
 def root():
-    return {"message":"alive"}
-
-
+    return {"message": "alive"}
 
 
 @app.post("/predict")
-def make_prediction(data:Data):
+def make_prediction(data: Data):
+    json_item = jsonable_encoder(data)
+    df_new = pd.DataFrame(json_item, index=[0])
+    df_write = preprocessor(df_new)
+    a=loaded_model.predict(df_write)[0]
     
+    return {"Churn":f"{a}"}
 
-    return  data
